caf3310d3183a52c2d958c2bea3778370bc15786
keil <38099918+zzzkeil@users.noreply.github.com>
2019-02-05 11:05:34 +0100

    Create pi_kiosk.sh

---
+++ pi_kiosk.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#### install stuff
+sudo apt-get install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser -y
+echo
+echo
+
+#### choose site
+read -p "Welche Internetseite soll aufgerufen werden ? >>>>  " -e -i https://zeroaim.de showurl
+echo
+echo
+
+#### openbox config
+sudo mv /etc/xdg/openbox/autostart /etc/xdg/openbox/autostart.orig
+echo "
+#Disable screen saver / screen blanking / power management
+xset s off
+xset s noblank
+xset -dpms
+
+#Exit X server with CTRL-ATL-Backspace
+setxkbmap -option terminate:ctrl_alt_bksp
+
+#Chromium incognito kiosk mode
+sed -i "'"s/"exited_cleanly":false/"exited_cleanly":true/"'" ~/.config/chromium/"'"Local State"'"
+sed -i "'"s/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/"'" ~/.config/chromium/Default/Preferences
+chromium-browser --incognito --disable-infobars --kiosk "'"$showurl"'"
+ " | sudo tee --append /etc/xdg/openbox/autostart > /dev/null
+echo 
+echo
+
+#### slightly more secure user pi
+sudo mv /etc/sudoers.d/010_pi-nopasswd /etc/sudoers.d/010_pi-nopasswd.orig
+echo "
+pi ALL=(ALL) PASSWD: ALL
+" | sudo tee --append /etc/sudoers.d/010_pi-nopasswd > /dev/null
+echo
+echo
+
+#### autostart openbox/chrome
+sudo cp /etc/profile /etc/profile.orig
+echo "startx -- -nocursor"  | sudo tee --append /etc/profile > /dev/null
+echo
+echo
+
+#### autologing pi without raspi-config
+# next version
