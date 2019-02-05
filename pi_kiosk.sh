4bcf96ede0e4779b9f3e843102dd68d93092b0ac
keil <38099918+zzzkeil@users.noreply.github.com>
2019-02-05 15:59:34 +0100

    Update pi_kiosk.sh

--- pi_kiosk.sh
+++ pi_kiosk.sh
@@ -17,7 +17,7 @@ setxkbmap -option terminate:ctrl_alt_bksp
 #Chromium incognito kiosk mode
 sed -i '"'s/"exited_cleanly":false/"exited_cleanly":true/'"' ~/.config/chromium/'"'Local State'"'
 sed -i '"'s/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/'"' ~/.config/chromium/Default/Preferences
-chromium-browser --incognito --disable-infobars --kiosk "'"'""${showurl}""'"'"
+chromium-browser --incognito --disable-infobars --hide-scrollbars --kiosk "'"'""${showurl}""'"'"
  " | sudo tee --append /etc/xdg/openbox/autostart > /dev/null
  exit 1
 else
