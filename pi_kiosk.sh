ea3ce367d507625b7f4d313d2b81ed33b953bf78
keil <38099918+zzzkeil@users.noreply.github.com>
2019-02-05 15:57:30 +0100

    Update pi_kiosk.sh

--- pi_kiosk.sh
+++ pi_kiosk.sh
@@ -1,17 +1,40 @@
 #!/bin/bash
 
 
-#### install stuff
-read -p "apt update needed ?  [yn]" -n 1 -r answer1
+read -p "Just change the Site?  [yn]" -n 1 -r answer1
 if [[ $answer1 = y ]]; then
+read -p "Welche Internetseite soll aufgerufen werden ?  " -e -i https://zeroaim.de showurl
+sudo mv /etc/xdg/openbox/autostart /etc/xdg/openbox/autostart.orig
+echo "
+#Disable screen saver / screen blanking / power management
+xset s off
+xset s noblank
+xset -dpms
+
+#Exit X server with CTRL-ATL-Backspace
+setxkbmap -option terminate:ctrl_alt_bksp
+
+#Chromium incognito kiosk mode
+sed -i '"'s/"exited_cleanly":false/"exited_cleanly":true/'"' ~/.config/chromium/'"'Local State'"'
+sed -i '"'s/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/'"' ~/.config/chromium/Default/Preferences
+chromium-browser --incognito --disable-infobars --kiosk "'"'""${showurl}""'"'"
+ " | sudo tee --append /etc/xdg/openbox/autostart > /dev/null
+ exit 1
+else
+#### choose site
+read -p "Welche Internetseite soll aufgerufen werden ?  " -e -i https://zeroaim.de showurl
+echo
+echo
+fi
+
+
+
+#### install stuff
+read -p "apt update needed ?  [yn]" -n 1 -r answer2
+if [[ $answer2 = y ]]; then
 sudo apt update && sudo apt upgrade -y
 fi
 sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser -y
-echo
-echo
-
-#### choose site
-read -p "Welche Internetseite soll aufgerufen werden ?  " -e -i https://zeroaim.de showurl
 echo
 echo
 
@@ -29,7 +52,7 @@ setxkbmap -option terminate:ctrl_alt_bksp
 #Chromium incognito kiosk mode
 sed -i '"'s/"exited_cleanly":false/"exited_cleanly":true/'"' ~/.config/chromium/'"'Local State'"'
 sed -i '"'s/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/'"' ~/.config/chromium/Default/Preferences
-chromium-browser --incognito --disable-infobars --kiosk "'"'""${showurl}""'"'"
+chromium-browser --incognito --disable-infobars --hide-scrollbars --kiosk "'"'""${showurl}""'"'"
  " | sudo tee --append /etc/xdg/openbox/autostart > /dev/null
 echo 
 echo
