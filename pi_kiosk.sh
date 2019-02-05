67cce3a77126a64502ca7c0679edf8540307f5cc
keil <38099918+zzzkeil@users.noreply.github.com>
2019-02-05 11:47:59 +0100

    Update pi_kiosk.sh

--- pi_kiosk.sh
+++ pi_kiosk.sh
@@ -13,7 +13,7 @@ echo
 
 #### openbox config
 sudo mv /etc/xdg/openbox/autostart /etc/xdg/openbox/autostart.orig
-echo '
+echo "
 #Disable screen saver / screen blanking / power management
 xset s off
 xset s noblank
@@ -23,10 +23,10 @@ xset -dpms
 setxkbmap -option terminate:ctrl_alt_bksp
 
 #Chromium incognito kiosk mode
-sed -i "'"s/"exited_cleanly":false/"exited_cleanly":true/"'" ~/.config/chromium/"'"Local State"'"
-sed -i "'"s/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/"'" ~/.config/chromium/Default/Preferences
-chromium-browser --incognito --disable-infobars --kiosk "'"$showurl"'"
- ' | sudo tee --append /etc/xdg/openbox/autostart > /dev/null
+sed -i '"'s/"exited_cleanly":false/"exited_cleanly":true/'"' ~/.config/chromium/'"#Local State'"'
+sed -i '"'s/"exited_cleanly":false/"exited_cleanly":true/; s/"exit_type":"[^"]\+"/"exit_type":"Normal"/'"' ~/.config/chromium/Default/Preferences
+chromium-browser --incognito --disable-infobars --kiosk '"'$showurl'"'
+ " | sudo tee --append /etc/xdg/openbox/autostart > /dev/null
 echo 
 echo
 
