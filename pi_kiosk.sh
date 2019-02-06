5ce26e81234407a5cbd1b1389a8f9185369870e0
keil <38099918+zzzkeil@users.noreply.github.com>
2019-02-06 08:50:01 +0100

    Update pi_kiosk.sh

--- pi_kiosk.sh
+++ pi_kiosk.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-
-read -p "Just change the Site?  [yn]" -n 1 -r answer1
+ 
+read -p "Just change the website?  [yn]" -n 1 -r answer1
+echo "When you run this script for the first time, reply with n"
 if [[ $answer1 = y ]]; then
-read -p "Welche Internetseite soll aufgerufen werden ?  " -e -i https://zeroaim.de showurl
+read -p "Which website should be opened : " -e -i https://zeroaim.de showurl
 sudo mv /etc/xdg/openbox/autostart /etc/xdg/openbox/autostart.orig
 echo "
 #Disable screen saver / screen blanking / power management
@@ -21,20 +22,25 @@ chromium-browser --incognito --disable-infobars --hide-scrollbars --kiosk "'"'""
  " | sudo tee --append /etc/xdg/openbox/autostart > /dev/null
  exit 1
 else
-#### choose site
-read -p "Welche Internetseite soll aufgerufen werden ?  " -e -i https://zeroaim.de showurl
-echo
-echo
+read -p "Just change touchscreen orientation to normal?  [yn]" -n 1 -r answer2
+echo "When you run this script for the first time, reply with n"
+if [[ $answer2 = y ]]; then
+sudo rm /usr/share/X11/xorg.conf.d/45-evdev.conf
+ exit 1
+fi
 fi
 
-
+#### choose site
+read -p "Which website should be opened :  " -e -i https://zeroaim.de showurl
+echo
+echo
 
 #### install stuff
-read -p "apt update needed ?  [yn]" -n 1 -r answer2
-if [[ $answer2 = y ]]; then
+read -p "apt update needed ?  [yn]" -n 1 -r answer3
+if [[ $answer3 = y ]]; then
 sudo apt update && sudo apt upgrade -y
 fi
-sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox chromium-browser -y
+sudo apt install --no-install-recommends xserver-xorg x11-xserver-utils xserver-xorg-input-evdev xinit openbox chromium-browser -y
 echo
 echo
 
@@ -57,6 +63,55 @@ chromium-browser --incognito --disable-infobars --hide-scrollbars --kiosk "'"'""
 echo 
 echo
 
+#### change touchscreen orientation
+read -p "Change touchscreen orientation 90 degrees ?  [yn]" -n 1 -r answer4
+echo "When you set this, add this display_hdmi_rotate=1  !manually! to /boot/config.txt"
+if [[ $answer4 = y ]]; then
+sudo mv /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/10-evdev.conf.orig
+sudo rm /usr/share/X11/xorg.conf.d/10-evdev.conf
+echo '
+Section "InputClass"
+        Identifier "evdev pointer catchall"
+        MatchIsPointer "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "evdev"
+EndSection
+
+Section "InputClass"
+        Identifier "evdev keyboard catchall"
+        MatchIsKeyboard "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "evdev"
+EndSection
+
+Section "InputClass"
+        Identifier "evdev touchpad catchall"
+        MatchIsTouchpad "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "evdev"
+EndSection
+
+Section "InputClass"
+        Identifier "evdev tablet catchall"
+        MatchIsTablet "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "evdev"
+EndSection
+
+Section "InputClass"
+        Identifier "evdev touchscreen catchall"
+        MatchIsTouchscreen "on"
+        MatchDevicePath "/dev/input/event*"
+        Driver "evdev"
+        Option "SwapAxes" "true"
+        Option "InvertX" "false"
+        Option "InvertY" "true"
+EndSection
+' | | sudo tee --append /usr/share/X11/xorg.conf.d/45-evdev.conf > /dev/null
+fi
+echo
+echo
+
 #### slightly more secure user pi
 sudo mv /etc/sudoers.d/010_pi-nopasswd /etc/sudoers.d/010_pi-nopasswd.orig
 echo "
@@ -67,13 +122,18 @@ echo
 
 #### autostart openbox/chrome
 #### autostart question temp
-read -p "Setup autostart ?  [yn]" -n 1 -r answer1
-if [[ $answer1 = y ]]; then
+read -p "Setup autostart ?  [yn]" -n 1 -r answer5
+if [[ $answer5 = y ]]; then
 sudo cp /etc/profile /etc/profile.orig
 echo "startx -- -nocursor"  | sudo tee --append /etc/profile > /dev/null
 fi
 echo
 echo
 
-#### autologing pi without raspi-config
-# next version
+
+#### todo 4  next versions
+## autologing pi without raspi-config
+## check if autostart arealy set and skip
+## change / vars for screen oriantations
+##
+##
